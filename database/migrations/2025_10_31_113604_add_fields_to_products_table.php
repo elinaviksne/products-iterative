6c3266001df488ad506f22928219079f109e621d
testuser <test@test.test>
2025-10-31 13:38:18 +0200

    added more fields to products

---
+++ database/migrations/2025_10_31_113604_add_fields_to_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('quantity')->default(0);
+            $table->date('expiration_date')->nullable();
+            $table->enum('status', ['available', 'unavailable'])->default('available');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn(['quantity', 'expiration_date', 'status']);
+        });
+    }
+};
