f8b1549af11849f7c49663c6ba1f4f5914f99be8
testuser <test@test.test>
2025-10-31 15:03:37 +0200

    added the routes to increase and decrease functions

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\ProductController;
 
 Route::redirect('/', '/products');
 
-
 Route::resource('products', ProductController::class);
 
-
+// Quantity funkcionalitāte
+Route::post('products/{product}/increase', [ProductController::class, 'increase'])->name('products.increase');
+Route::post('products/{product}/decrease', [ProductController::class, 'decrease'])->name('products.decrease');
