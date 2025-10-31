6a22ffe6a2d6096fefdee9dbe846b99837f77f6c
testuser <test@test.test>
2025-10-31 14:50:04 +0200

    added redirect route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
+Route::redirect('/', '/products');
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('products', ProductController::class);
+
+
