283de8a710a9cb4274773cfb72fa6e505b8a288a
testuser <test@test.test>
2025-10-31 13:12:23 +0200

    edited the schema values

--- database/migrations/2025_10_31_110240_create_products_table.php
+++ database/migrations/2025_10_31_110240_create_products_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
-            $table->int('price');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);
             $table->timestamps();
         });
     }
