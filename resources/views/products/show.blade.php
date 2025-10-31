da661765830c469b2a6e5cf028025cbdb4801b3c
testuser <test@test.test>
2025-10-31 15:04:44 +0200

    added the functionality of increase and decrease to show view

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,7 +2,20 @@
     <h1>{{ $product->name }}</h1>
 
     <p><strong>Apraksts:</strong> {{ $product->description }}</p>
-    <p><strong>Cena:</strong> {{ $product->price }} €</p>
+    <p><strong>Cena:</strong> €{{ $product->price }}</p>
+    <p><strong>Daudzums noliktavā:</strong> {{ $product->quantity }}</p>
+    <p><strong>Statuss:</strong> {{ $product->status }}</p>
+    <p><strong>Derīguma termiņš:</strong> {{ $product->expiration_date }}</p>
 
-    <a href="{{ route('products.index') }}">← Atpakaļ uz sarakstu</a>
+    <form action="{{ route('products.increase', $product) }}" method="POST" style="display:inline;">
+        @csrf
+        <button type="submit">+ Palielināt</button>
+    </form>
+
+    <form action="{{ route('products.decrease', $product) }}" method="POST" style="display:inline;">
+        @csrf
+        <button type="submit">− Samazināt</button>
+    </form>
+
+    <p><a href="{{ route('products.index') }}">Atpakaļ uz produktu sarakstu</a></p>
 </x-layout>
